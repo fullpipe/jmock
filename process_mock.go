64afb67880aec6615a35753e8e90b7e8afd49643
Eugene Bravov <eugene.bravov@gmail.com>
2021-09-20 20:40:15 +0300

    Update process_mock.go

--- process_mock.go
+++ process_mock.go
@@ -11,46 +11,8 @@ import (
 
 // ProcessMock writes from mock to ResponseWriter
 func ProcessMock(w http.ResponseWriter, r *http.Request, mock *Mock) error {
-	body := getBodyCopy(r)
-
 	if mock.Proxy != "" {
-		pr, _ := http.NewRequest(r.Method, r.URL.String(), ioutil.NopCloser(bytes.NewBuffer(body)))
-		pr.Header = r.Header
-
-		pr.URL.Host = mock.Proxy
-		pr.URL.Scheme = r.URL.Scheme
-		if strings.Contains(mock.Proxy, "://") {
-			purl, err := url.Parse(mock.Proxy)
-			if err != nil {
-				return err
-			}
-
-			pr.URL.Host = purl.Host
-			pr.URL.Scheme = purl.Scheme
-		}
-
-		if pr.URL.Scheme == "" {
-			pr.URL.Scheme = "http"
-		}
-
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		client := &http.Client{Transport: tr}
-		resp, err := client.Do(pr)
-		if err != nil {
-			return err
-		}
-
-		w.WriteHeader(resp.StatusCode)
-		for k := range resp.Header {
-			w.Header().Set(k, resp.Header.Get(k))
-		}
-
-		pb, _ := ioutil.ReadAll(resp.Body)
-		w.Write(pb)
-
-		return nil
+		return proxyRequest(w, r, mock)
 	}
 
 	if nil != mock.Response.Headers {
@@ -59,25 +21,71 @@ func ProcessMock(w http.ResponseWriter, r *http.Request, mock *Mock) error {
 		}
 	}
 
-	if mock.Response.JSON != nil && mock.Response.Body == nil {
-		w.Header().Set("Content-Type", "application/json")
-	}
-
 	if mock.Response.Code != nil {
 		w.WriteHeader(*mock.Response.Code)
 	}
 
 	if mock.Response.Body != nil {
-		w.Write([]byte(*mock.Response.Body))
+		_, err := w.Write([]byte(*mock.Response.Body))
+		if err != nil {
+			return err
+		}
 
 		return nil
 	}
 
 	if mock.Response.JSON != nil {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write(*mock.Response.JSON)
+		_, err := w.Write(*mock.Response.JSON)
+		if err != nil {
+			return err
+		}
 
 		return nil
+	}
+
+	return nil
+}
+
+func proxyRequest(w http.ResponseWriter, r *http.Request, mock *Mock) error {
+	body := getBodyCopy(r)
+	pr, _ := http.NewRequest(r.Method, r.URL.String(), ioutil.NopCloser(bytes.NewBuffer(body)))
+	pr.Header = r.Header
+
+	pr.URL.Host = mock.Proxy
+	pr.URL.Scheme = r.URL.Scheme
+	if strings.Contains(mock.Proxy, "://") {
+		purl, err := url.Parse(mock.Proxy)
+		if err != nil {
+			return err
+		}
+
+		pr.URL.Host = purl.Host
+		pr.URL.Scheme = purl.Scheme
+	}
+
+	if pr.URL.Scheme == "" {
+		pr.URL.Scheme = "http"
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(pr)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	for k := range resp.Header {
+		w.Header().Set(k, resp.Header.Get(k))
+	}
+
+	pb, _ := ioutil.ReadAll(resp.Body)
+	_, err = w.Write(pb)
+	if err != nil {
+		return err
 	}
 
 	return nil
